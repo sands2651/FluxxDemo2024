8500788ad09389173a38012adec954c15524de36
Sandeep Kasi <skasi@galaxe.com>
2024-08-05 17:05:53 +0530

    updatedcode

--- src/granteeportal/Demoportal.java
+++ src/granteeportal/Demoportal.java
@@ -4,7 +4,7 @@ public class Demoportal
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Grantee Portal");
+		System.out.println("Welcome to Fluxx Grantee Portal");
 	}
 
 }
